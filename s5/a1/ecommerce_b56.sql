44276787fadf30faa0afbe5b7ae73771b5082b3d
Arwyne De Guzman <arwynedeguzman@gmail.com>
2020-04-16 14:32:44 +0800

    this version includes activity 1 in s5

---
+++ s5/a1/ecommerce_b56.sql
@@ -0,0 +1,189 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 16, 2020 at 08:28 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecommerce_b56`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` decimal(11,2) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `category_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_orders`
+--
+
+CREATE TABLE `item_orders` (
+  `id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `purchase_date` date NOT NULL,
+  `transaction_code` varchar(255) NOT NULL,
+  `total` decimal(11,2) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id_fk` (`category_id`);
+
+--
+-- Indexes for table `item_orders`
+--
+ALTER TABLE `item_orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id_fk` (`order_id`),
+  ADD KEY `item_id_fk` (`item_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id_fk` (`user_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `item_orders`
+--
+ALTER TABLE `item_orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `category_id_fk` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `item_orders`
+--
+ALTER TABLE `item_orders`
+  ADD CONSTRAINT `item_id_fk` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_id_fk` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ s5/musicstore.sql
@@ -0,0 +1,59 @@
+/*
+	SQL - Structured Query Language
+		- SQL is domain-specific language, used in programming and design for managing data that is held in a RDBMS
+
+	DATA TYPES
+		- description, or the types of the data that goes in our table
+		
+		Music Store
+		   Artists
+		| id | name |
+
+		* What type of data is appropriate for the ID / Name field?
+		* What data are we expecting to save in these fields?
+
+		Artists
+		| id (integer/numeric) | name (string) |
+
+		Albums
+		| id (integer/numeric) | name(string) | year (date)
+
+
+	SQL DATA TYPES
+		Strings:
+
+		CHAR(s) - fixed length string w/ sizes up to 255 (bytes)
+		VARCHAR(s) - variable length string w/ sizes up to 65, 535 (bytes)
+		TEXT - variable length string w/ sizes to 65, 535 (bytes)
+
+		The way they differ is how they are stored in the database.
+		Ideally, CHAR for 1-2words only, VARCHAR for 3-10 words, TEXT for paragraphs
+
+
+		Integer/Numeric:
+
+		TINYINT = min of -128 values, max of 127 values
+		SMALLINT = min of -32,768 values, max of 32, 767
+		INT = min of -2,147,483,648 values, max of 2,147,483,647
+			ex. Primary Key, Foreign Key
+		BIGINT = min of -9,223,372,036,854,755,808 values, max of 9,223,372,036,854,755,807
+		DECIMAL(M,D) = M stands if the total number of digits and D is the number of digits after the decimal point
+			ex. DECIMAL (4,2) = 1,450.75 
+
+
+		DATE and TIME
+		DATE = HH:MM:SS
+		DATETIME/TIMESTAMP = YYYY-MM-DD HH:MM:SS
+
+		*DATA DEFINITION LANGUAGE
+		CREATE DATABASE
+		CREATE TABLE
+			-DATA TYPES
+			-FOREIGN KEYS, CONSTRAINT
+		ALTER TABLE
+			-ADD COLUMN
+			-DROP COLUMN
+			-RENAME COLUMN
+		DROP TABLE
+		RENAME TABLE
+*/
